7be216700739381d33078be29c03fd6c3c6a14d6
omamoh18 <goku-legend@outlook.com>
2020-02-22 16:26:44 +0100

    Add project folders and files

---
+++ src/test/java/basicTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class basicTest {
+    @Test
+    public void test(){
+
+        assertEquals(1+2, 3);
+    }
+}
